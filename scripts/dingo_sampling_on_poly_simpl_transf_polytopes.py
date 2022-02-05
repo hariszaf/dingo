887ea64d01db7e02c6b9139cde08a047e0982d7a
hariszaf <haris-zaf@hcmr.gr>
2022-02-05 18:12:51 +0100

    map back to the reactions dim

--- scripts/dingo_sampling_on_poly_simpl_transf_polytopes.py
+++ scripts/dingo_sampling_on_poly_simpl_transf_polytopes.py
@@ -7,18 +7,18 @@ from time import process_time
 import pickle
 import multiprocessing
 
-def sample_on_polyround_processed_polytope(network):
+def sample_on_polyround_processed_polytope(input_polytope):
 
    current_directory = os.getcwd()
-   name              = network.split("/")[-1]
-   polytope_file     = network
+   name              = input_polytope.split("/")[-1]
+   polytope_file     = input_polytope
 
    with open(polytope_file, "rb") as f:
       obj = pickle.load(f)
-   polytope = obj[0]
+   simpl_polytope = obj[0]
 
-   A = polytope.A.to_numpy()
-   b = polytope.b.to_numpy()
+   A = simpl_polytope.A.to_numpy()
+   b = simpl_polytope.b.to_numpy()
 
    res = gmscale(A, 0.99)
    res = apply_scaling(A, b, res[0], res[1])
@@ -28,8 +28,8 @@ def sample_on_polyround_processed_polytope(network):
 
 
    # Sample from the polytope built using the parrallel MMCS 
-   start = process_time() 
-   steady_states = PolytopeSampler.sample_from_polytope(scaled_A, 
+   start         = process_time() 
+   dingo_samples = PolytopeSampler.sample_from_polytope(scaled_A, 
 							scaled_b,
                                                    	psrf = True, 
                                                    	parallel_mmcs = False
@@ -39,8 +39,15 @@ def sample_on_polyround_processed_polytope(network):
    
    print("Dingo for the " + name + " model took " + str(sampling_runtime) + " sec, using time.procesess_time(), to sample using polyround processed polytope")
 
-   with open("../steady_states/dingo_polyround" + name + ".pckl", "wb") as dingo_steadystates_file: 
-         pickle.dump(steady_states, dingo_steadystates_file)
+   with open("../steady_states/dingo_samples_" + name + ".pckl", "wb") as dingo_samples_file: 
+         pickle.dump(dingo_samples, dingo_samples_file)
+
+
+   steady_states = simpl_polytope.back_transform(dingo_samples)
+   with open("../steady_states/dingo_steady_states_" + name + ".pckl", "wb") as dingo_steadystates_file:
+      pickle.dump(steady_states, dingo_steadystates_file)
+
+
 
 if __name__ == '__main__':
 
