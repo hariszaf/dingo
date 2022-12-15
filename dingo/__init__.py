7d60d85e6b1e83895a6707cee490a81b844cae9e
hariszaf <haris.zafeiropoulos@kuleuven.be>
2022-12-15 19:59:57 +0100

    valid samples for sars in notebook; add read files in init

--- dingo/__init__.py
+++ dingo/__init__.py
@@ -11,7 +11,7 @@ import os
 import pickle
 from dingo.fva import slow_fva
 from dingo.fba import slow_fba
-from dingo.loading_models import read_json_file
+from dingo.loading_models import read_json_file, read_mat_file, read_sbml_file
 from dingo.inner_ball import slow_inner_ball
 from dingo.nullspace import nullspace_dense, nullspace_sparse
 from dingo.scaling import gmscale
